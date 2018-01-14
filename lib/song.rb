eccd548c4cc6441a7c48e57d35b791db4ce47398
Wayne Buxton <waynebuxton2184@yahoo.com>
2018-01-14 17:54:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,6 +31,8 @@ attr_accessor :name, :artist, :genre
     genre_count = {}
     @@genres.each do |genre|
       if genre_count[genre] = 1 
+        true
+      else genre_count.include? += 1
 
       #binding.pry
     end
