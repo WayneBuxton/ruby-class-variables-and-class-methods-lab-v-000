e3386ac99a26672b4e5ae39b663eb2780c3db97d
Wayne Buxton <waynebuxton2184@yahoo.com>
2018-01-14 16:52:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,7 @@ attr_accessor :name, :artist, :genre
   def self.genre_count
     genre_count = {}
     @@genres.each do |key,value|
-      genre_count[:genre]
+      genre_count[:key]
 
       binding.pry
     end
