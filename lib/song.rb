df4a37da7df88df02c355e37996ba041b115f11e
Wayne Buxton <waynebuxton2184@yahoo.com>
2018-01-14 02:04:16 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,12 @@
+require "pry"
+class Song
+attr_accessor :name, :artist, :genre
+@@count = 0
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count
+  end
+
+end
